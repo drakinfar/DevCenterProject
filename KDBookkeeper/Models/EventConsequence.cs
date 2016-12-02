2943c38cf7cb6c10cdc74188e6c6e321736908dd
jeremy <jerem@JEREMYHOLLEY>
2016-12-02 15:14:04 -0700

    small db change to add event sonsequences to allow event to trigger settlement/survivor changes

---
+++ KDBookkeeper/Models/EventConsequence.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace KDBookkeeper.Models
+{
+	public class EventConsequence
+	{
+		public int EventConsequenceId { get; set; }
+
+		public int LanternEventId { get; set; }
+
+		public virtual LanternEvent LanternEvent { get; set; }
+
+		public int ConsequenceType { get; set; } //the type of consequence i.e quarry, nemisis, etc
+
+		public int ConsequenceObjectId { get; set; } //the id of the consequence to help look up.
+	}
+}
--- KDBookkeeper/Models/LanternEvent.cs
+++ KDBookkeeper/Models/LanternEvent.cs
@@ -3,22 +3,24 @@ using System.Collections.Generic;
 
 namespace KDBookkeeper.Models
 {
-    public partial class LanternEvent
-    {
-        public LanternEvent()
-        {
-            GameEvents = new HashSet<GameEvents>();
-            SettlementMileStones = new HashSet<SettlementMileStones>();
-        }
+	public partial class LanternEvent
+	{
+		public LanternEvent()
+		{
+			GameEvents = new HashSet<GameEvents>();
+			SettlementMileStones = new HashSet<SettlementMileStones>();
+			EventConsequences = new HashSet<EventConsequence>();
+		}
 
-        public int Id { get; set; }
-        public string Name { get; set; }
-        public string Description { get; set; }
-        public int LaternYear { get; set; }
-        public string Expansion { get; set; }
-        public bool IsRandom { get; set; }
+		public int Id { get; set; }
+		public string Name { get; set; }
+		public string Description { get; set; }
+		public int LaternYear { get; set; }
+		public string Expansion { get; set; }
+		public bool IsRandom { get; set; }
 
-        public virtual ICollection<GameEvents> GameEvents { get; set; }
-        public virtual ICollection<SettlementMileStones> SettlementMileStones { get; set; }
-    }
+		public virtual ICollection<GameEvents> GameEvents { get; set; }
+		public virtual ICollection<SettlementMileStones> SettlementMileStones { get; set; }
+		public virtual ICollection<EventConsequence> EventConsequences { get; set; }
+	}
 }
